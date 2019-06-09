f72a587b71c071427000f3051d73cf4201646011
dongphan1988 <phantrongdong15488@gmail.com>
2019-06-09 18:41:50 +0700

    change color

---
+++ src/app/change-coler/change-coler.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChangeColerComponent } from './change-coler.component';
+
+describe('ChangeColerComponent', () => {
+  let component: ChangeColerComponent;
+  let fixture: ComponentFixture<ChangeColerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ChangeColerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChangeColerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/change-coler/change-coler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-change-coler',
+  templateUrl: './change-coler.component.html',
+  styleUrls: ['./change-coler.component.scss']
+})
+export class ChangeColerComponent implements OnInit {
+color:string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
